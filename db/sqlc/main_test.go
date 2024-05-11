72448386eb35e5c30e1fe416a25f312d7131f067
N17Ali <Ali.naderi.n7@gmail.com>
2024-05-11 16:46:48 +0330

    simple bank api. Transfer tx added.

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgres://postgres:secret@localhost:5432/bank?sslmode=disable"
+)
+
+var testQueries *Queries
+var testDb *sql.DB
+
+func TestMain(m *testing.M) {
+	var err error
+	testDb, err = sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db: ", err)
+	}
+	defer testDb.Close()
+
+	testQueries = New(testDb)
+
+	os.Exit(m.Run())
+}
